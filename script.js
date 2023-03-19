4f93438603ed4237f589da2fb3d1aed665fe6776
Samuel Alves <samueltheodoro21@gmail.com>
2023-03-19 20:29:46 -0300

    responsive notebook update

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const general = () => {
   const sections = document.querySelectorAll('.sections')
 
-  sections.forEach((section) => {
-    if (window.innerWidth < 900) {
-      section.style.height = `${(window.innerHeight * 80) / 100}px`
-    } else {
-      section.style.height = `${(window.innerHeight * 90) / 100}px`
-    }
-  })
-
-  const handleResize = () => {
+  const defineSectionHeight = () => {
     sections.forEach((section) => {
       if (window.innerWidth < 900) {
-        section.style.height = `${(window.innerHeight * 80) / 100}px`
+        if (
+          section.classList.contains('section04') ||
+          section.classList.contains('section05')
+        ) {
+          section.style.height = `${(window.innerHeight * 50) / 100}px`
+        } else {
+          section.style.height = `${(window.innerHeight * 80) / 100}px`
+        }
       } else {
-        section.style.height = `${(window.innerHeight * 90) / 100}px`
+        if (
+          section.classList.contains('section04') ||
+          section.classList.contains('section05')
+        ) {
+          section.style.height = `${(window.innerHeight * 50) / 100}px`
+        } else {
+          section.style.height = `${(window.innerHeight * 90) / 100}px`
+        }
       }
     })
   }
-
-  window.onresize = handleResize
+  defineSectionHeight()
+  window.onresize = defineSectionHeight
 }
 
 general()
